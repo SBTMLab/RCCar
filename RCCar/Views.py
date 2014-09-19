d6fed963540c4d7b9f9b14d2ba9fd04696aebc7b
Pusnow <pusnow@me.com>
2014-09-19 14:43:00 +0900

    기본 골격 작성

---
+++ RCCar/Views.py
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 -*-
+from RCCar import app
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify
+
+
+@app.route('/')
+def index():
+    return "Hello World!"
