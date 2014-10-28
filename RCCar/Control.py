b4b1babd7bccb8aef47d1fce5c701b2271ba0eb0
Pusnow <pusnow@me.com>
2014-10-28 15:51:30 +0900

    speed test

--- RCCar/Control.py
+++ RCCar/Control.py
@@ -2,6 +2,13 @@
 from RPIO import PWM
 
 servo = PWM.Servo()
+RPIO.setup(27, RPIO.OUT)
+dcen = 27
+RPIO.setup(22, RPIO.OUT)
+dcdr = 22
+
+dc = PWM.Servo()
+
 
 class Control :
 	Direction = 0
@@ -28,11 +35,22 @@ class Control :
 	@staticmethod
 	def speedup(val):
 		Control.Speed += val
+		Control.setspeed()
 
 	@staticmethod
 	def speeddown(val):
 		Control.Speed -= val
+		Control.setspeed()
 
 	@staticmethod
 	def setdirection():
 		servo.set_servo(8,1400 + Control.Direction)
+
+
+	@staticmethod
+	def setspeed():
+		RPIO.output(dcen, False)
+		RPIO.output(dcdr, False)
+		servo.set_servo(10, 2000)
+
+
